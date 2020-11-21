92311bd14fbe515052650c7266a72a06198ff9cc
zapryan0v <zaprqnov.zaprqn@gmail.com>
2020-11-22 01:06:50 +0200

    add some style for responsive

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -2,16 +2,15 @@ var counters = document.querySelectorAll('.counter');
 
 counters.forEach(counter => {
 	var updateCount = () => {
-	var target = +counter.getAttribute('data-target');
-	var count = +counter.innerText;
+		var target = +counter.getAttribute('data-target');
+		var count = +counter.innerText;
 
-		if (count < target) {
-			counter.innerText = count + 1;
-			setInterval(updateCount, 1);
-		} else {
-			counter.innerText = target;
-			}
-		};
-
+			if (count < target) {
+				counter.innerText = count + 1;
+				setInterval(updateCount,300);
+			} else {
+				counter.innerText = target;
+				}
+	};
 updateCount();
 });
